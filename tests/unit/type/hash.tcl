6e5e8dd3b76a73842d70a8d592d138d44271a8de
jonghoonpark <dev@jonghoonpark.com>
2024-04-15 16:18:33 +0900

    try to set hash-max-listpack-value

--- tests/unit/type/hash.tcl
+++ tests/unit/type/hash.tcl
@@ -484,11 +484,9 @@ start_server {tags {"hash"}} {
 
     # hashtable {{a 1} {b 2} {c 3} {d 4} {e 5} {6 f} {7 g} {8 h} {9 i} {[randstring 70 90 alpha] 10}}
     # listpack {{a 1} {b 2} {c 3} {d 4} {e 5} {6 f} {7 g} {8 h} {9 i} {10 j}}
-    # Expected '*hashtable*' to equal or match
-    # 'Value at:0x7f50b95df180 refcount:1 encoding:listpack serializedlength:158 lru:1878956 lru_seconds_idle:0'
+
     test {Is a ziplist encoded Hash promoted on big payload?} {
-        set original_max_value [lindex [r config get hash-max-ziplist-value] 1]
-        r config set hash-max-listpack-value $original_max_value
+        r config set hash-max-listpack-value 64
 
         r hset smallhash foo [string repeat a 1024]
         r debug object smallhash
